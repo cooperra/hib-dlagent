f425f89c649d0025ecbb1d4e8639ca4aa3d67867
Robbie Cooper <cooperra@users.noreply.github.com>
2021-05-29 14:30:44 -0400

    Log out after finding the url (or on error)

--- discover-url.py
+++ discover-url.py
@@ -18,13 +18,20 @@ except humblebundle.exceptions.HumbleAuthenticationException as e:
     print(e, file=sys.stderr)
     exit()
 
-order_list = client.order_list()
-for order in order_list:
-    for subproduct in order.subproducts or []:
-        for download in subproduct.downloads or []:
-            for download_struct in download.download_struct or []:
-                url = download_struct.url.web
-                if url and filename in url:
-                    print(url)
-                    exit()
+def find_url(order_list, filename):
+    for order in order_list:
+        for subproduct in order.subproducts or []:
+            for download in subproduct.downloads or []:
+                for download_struct in download.download_struct or []:
+                    url = download_struct.url.web
+                    if url and filename in url:
+                        return url
+
+try:
+    order_list = client.order_list()
+    url = find_url(order_list, filename)
+    if url:
+        print(url)
+finally:
+    client.logout()
 
